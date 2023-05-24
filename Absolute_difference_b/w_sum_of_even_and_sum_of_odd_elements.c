7245ed06b0422bf3f540d28bb4acab73a3be600a
22MH1A05I4 <119779629+22MH1A05I4@users.noreply.github.com>
2023-05-24 14:16:23 +0530

    #include<stdio.h> #include<math.h> #include<stdlib.h> int main() {     int i,n,esum=0,osum=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             esum=esum+a[i];         }         else if(a[i]%2!=0)         {             osum=osum+a[i];         }     }     int diff=(osum-esum);     printf("%d",abs(diff)); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    int i,n,esum=0,osum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            esum=esum+a[i];
+        }
+        else if(a[i]%2!=0)
+        {
+            osum=osum+a[i];
+        }
+    }
+    int diff=(osum-esum);
+    printf("%d",abs(diff));
+}
